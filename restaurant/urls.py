a9c9f419e7aab745fec194755aea8ee5a89b4c6c
福井　一仁 <kazuto_0224@yahoo.co.jp>
2024-12-10 21:08:29 +0900

    10章最後まで

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -19,5 +19,12 @@ name="reservation_create"),
 name="reservation_list"),
     path('reservation-delete', views.reservation_delete,
 name='reservation_delete'),
+    path('review-list/<int:pk>/', views.ReviewListView.as_view(),
+name="review_list"),
+    path('review-create/<int:pk>/', views.ReviewCreateView.as_view(),
+name="review_create"),
+    path('review-update/<int:pk>/', views.ReviewUpdateView.as_view(),
+name="review_update"),
+    path('review-delete', views.review_delete, name='review_delete'),
 ]
 
